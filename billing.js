5577815bbfa0c6d0bec43107c4c55faf73623505
Gabriel C <gabriel@winterwind.com>
2020-09-25 00:08:49 +0800

    * fixed endpoint indentation

--- billing.js
+++ billing.js
@@ -2,7 +2,7 @@ import stripePackage from "stripe";
 import handler from "./libs/handler-lib";
 import {calculateCost} from "./libs/billing-lib";
 
-export const main = handler(async (event, conext) => {
+export const main = handler(async (event, context) => {
     const {storage, source} = JSON.parse(event.body);
     const amount = calculateCost(storage);
     const description = "Scratch charge";
